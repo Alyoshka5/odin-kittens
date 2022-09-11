f82c26f11ca0b7fdf50866562829eea19cea7d39
Alyoshka <alyoshkamyshko@gmail.com>
2022-09-11 16:37:30 -0700

    Create Kitten model and controller and controller methods

---
+++ app/controllers/kittens_controller.rb
@@ -0,0 +1,43 @@
+class KittensController < ApplicationController
+    def index
+        @kittens = Kitten.all
+    end
+
+    def show
+        @kitten = Kitten.find(params["id"])
+    end
+
+    def new
+        @kitten = Kitten.new
+    end
+
+    def create
+        @kitten = Kitten.new(kitten_params())
+
+        if @kitten.save
+            redirect_to root_path
+        else
+            render :new, :unprocessable_entity
+        end
+    end
+
+    def edit
+        @kitten = Kitten.find(params["id"])
+    end
+
+    def update
+        @kitten = Kitten.find(params["id"])
+
+        if @kitten.save
+            redirect_to root_path
+        else
+            render :edit, :unprocessable_entity
+        end
+    end
+
+
+    private
+    def kitten_params
+        params.require(:kitten).permit(:name, :age, :cuteness, :softness)
+    end
+end
