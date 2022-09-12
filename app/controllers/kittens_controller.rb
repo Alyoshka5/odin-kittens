c629558888020ec6b6f57d4561373d11f7d30921
Alyoshka <alyoshkamyshko@gmail.com>
2022-09-11 17:33:04 -0700

    Add flash messages to display notices and errors

--- app/controllers/kittens_controller.rb
+++ app/controllers/kittens_controller.rb
@@ -15,9 +15,9 @@ class KittensController < ApplicationController
         @kitten = Kitten.new(kitten_params())
 
         if @kitten.save
-            redirect_to @kitten
+            redirect_to @kitten, notice: "Kitten successfully created!"
         else
-            render :new, :unprocessable_entity
+            render :new, status: :unprocessable_entity
         end
     end
 
@@ -29,7 +29,7 @@ class KittensController < ApplicationController
         @kitten = Kitten.find(params[:id])
 
         if @kitten.update(kitten_params())
-            redirect_to @kitten
+            redirect_to @kitten, notice: "Kitten successfully updated!"
         else
             render :edit, status: :unprocessable_entity
         end
@@ -39,7 +39,7 @@ class KittensController < ApplicationController
         @kitten = Kitten.find(params[:id])
         @kitten.destroy
 
-        redirect_to root_path, status: :see_other
+        redirect_to root_path, status: :see_other, notice: "Kitten successfully deleted!"
     end
 
 
